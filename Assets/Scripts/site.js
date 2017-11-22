9dc8794f51ecf4f340c79172cc854f767f9c2409
Alex Ciampi <alexciampidesign@gmail.com>
2017-11-22 14:36:46 -0500

    Added some animation styles. Added js for when an element comes into view. Its temporary until i create common class to get added to each animated element.

--- Assets/Scripts/site.js
+++ Assets/Scripts/site.js
@@ -45,4 +45,46 @@ $(document).ready(function(){
             $navbar.toggleClass('open');
         }
     });
+
+    // When an element comes into viewport
+
+
+    function inView() {
+        $.fn.visible = function (partial) {
+    
+            var $t = $(this),
+                $w = $(window),
+                viewTop = $w.scrollTop(),
+                viewBottom = viewTop + $w.height(),
+                _top = $t.offset().top,
+                _bottom = _top + $t.height(),
+                compareTop = partial === true ? _bottom : _top,
+                compareBottom = partial === true ? _top : _bottom;
+    
+            return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    
+        };
+    
+        var win = $(window);
+    
+        var $topic = $(".topic");
+    
+        $topic.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("already-visible");
+            }
+        });
+    
+        win.scroll(function (event) {
+            $topic.each(function (i, el) {
+                var el = $(el);
+                if (el.visible(true)) {
+                    el.addClass("come-in");
+                }
+            });
+        });
+    };
+    inView();
+
 });
